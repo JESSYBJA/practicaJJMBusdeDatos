31e2396953a099d7a9e9649dbf21e6e51ff553d0
JESSYBJA <35872971+JESSYBJA@users.noreply.github.com>
2018-03-17 10:20:58 -0600

    codigo index.js
    nuevo

---
+++ arduinoBusdeDatos_nodejs/index.js
@@ -0,0 +1,37 @@
+const TelegramBot = require('node-telegram-bot-api');
+const serial = require ('serialport');
+//holaaaa
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '521700668:AAEWq0Pf7YWq5WVYPQuBnWsNiM0TjGRcaqg';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+var OpcionSerial = {
+  baudRate: 9600,
+  autoOpen: true
+}
+var Puerto = "/COMXX";
+var port = new serial (Puerto, OpcionSerial);
